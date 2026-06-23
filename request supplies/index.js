1fddaf2a066e62f21a9660176c3442a2c7287daf
fdf1338-ship-it <fdf1338@gmail.com>
2026-06-23 17:40:28 -0400

    Delete request supplies directory

--- request supplies/index.js
+++
@@ -1,8 +0,0 @@
-import { app } from "./config.js";
-
-console.log("Request Supplies Started");
-console.log(app);
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Page Loaded");
-});
